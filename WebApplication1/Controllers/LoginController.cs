fafc741f190c989661ab58c8f74f9cb5533b6c81
Nataly <nataly_moreno@umail.ucsb.edu>
2017-05-18 00:20:37 -0700

    Add session key to table

--- WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/Controllers/LoginController.cs
@@ -41,34 +41,48 @@ namespace WebApplication1.Controllers
                 return result;
             }
 
-            while (reader.Read())
+            reader.Read();
+            byte[] hashBytes = Convert.FromBase64String(reader["pass_hash"].ToString());
+            byte[] salt = new byte[16];
+            Array.Copy(hashBytes, 0, salt, 0, 16);
+            var pbkdf2 = new Rfc2898DeriveBytes(login.password, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+            for (int i = 0; i < 20; i++)
             {
-                byte[] hashBytes = Convert.FromBase64String(reader["pass_hash"].ToString());
-                byte[] salt = new byte[16];
-                Array.Copy(hashBytes, 0, salt, 0, 16);
-                var pbkdf2 = new Rfc2898DeriveBytes(login.password, salt, 10000);
-                byte[] hash = pbkdf2.GetBytes(20);
-                for (int i = 0; i < 20; i++)
+                //Wrong Password
+                if (hashBytes[i + 16] != hash[i])
                 {
-                    //Wrong Password
-                    if (hashBytes[i + 16] != hash[i])
-                    {
-                        result.message = INVALID_USERNAME_PASSWORD;
-                        Database.Disconnect();
-                        return result;
-                    }
+                    result.message = INVALID_USERNAME_PASSWORD;
+                    Database.Disconnect();
+                    return result;
                 }
-
-                byte[] sessionToken;
-                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-                rng.GetBytes(sessionToken = new byte[16]);
-
-                result.message = Convert.ToBase64String(sessionToken);
-                result.error = false;
             }
 
             Database.Disconnect();
+            byte[] sessionToken;
+            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+            rng.GetBytes(sessionToken = new byte[16]);
+
+            result.message = Convert.ToBase64String(sessionToken);
+            result.error = false;
+            SaveSessionKey(login.username, result.message);
             return result;
+        }
+
+        private void SaveSessionKey(string username, string sessionKey)
+        {
+            SqlCommand deleteUser = new SqlCommand("DELETE FROM Sessions WHERE username=@username;");
+            deleteUser.Parameters.AddWithValue("@username", username);
+            Database.Connect();
+            Database.Query(deleteUser);
+            Database.Disconnect();
+
+            SqlCommand insertUser = new SqlCommand("INSERT INTO Sessions VALUES(@username, @session_key);");
+            insertUser.Parameters.AddWithValue("@username", username);
+            insertUser.Parameters.AddWithValue("@session_key", sessionKey);
+            Database.Connect();
+            Database.Query(insertUser);
+            Database.Disconnect();
         }
     }
 }

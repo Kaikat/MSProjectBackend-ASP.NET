cbb59c82f8523531d4716fd4764de55e903720ac
Andrew Tran <andrew.tran345@gmail.com>
2017-05-19 02:47:00 -0700

    Started code for Get Player Animals

--- WebApplication1/Controllers/PlayerAnimalsController.cs
+++ WebApplication1/Controllers/PlayerAnimalsController.cs
@@ -14,5 +14,16 @@ namespace WebApplication1.Controllers
         Database Database = new Database();
 
         //TODO: Get Player Animals (caught/released/discovered)
+        // UNTESTED
+        [HttpGet] // All Animals for Player
+        public AnimalList Get()
+        {
+            // Prepare query for database
+            SqlCommand query = new SqlCommand("SELECT * FROM Animals WHERE username = @username;");
+            SqlDataReader reader = Database.Query("SELECT * FROM Animals WHERE username = @username;");
+            
+            AnimalList PlayerAnimals = new AnimalList();
+            return PlayerAnimals;
+        }
     }
 }
